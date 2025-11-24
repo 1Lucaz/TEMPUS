c075fbeb4984bd89e64b7ca8dfeb32f5cc68a601
1llucaz <lucasemanueloliveiraaraujo@gmail.com>
2025-11-24 12:59:36 -0300

    AJUSTE DE ROTAS

--- app/routers/api_router.py
+++ app/routers/api_router.py
@@ -1,9 +1,14 @@
 from fastapi import APIRouter
-from TEMPUS.modules import cliente_controller, servico_controller, funcionario_controller, ordem_servico_controller, item_servico_controller
+from TEMPUS.app.modules.cliente import cliente_router
+from TEMPUS.app.modules.funcionario import funcionario_router
+from TEMPUS.app.modules.item_servico import item_servico_router
+from TEMPUS.app.modules.ordem_servico import ordem_servico_router
+from TEMPUS.app.modules.servico import servico_router
+
 
 api_router = APIRouter()
-api_router.include_router(cliente_controller.router)
-api_router.include_router(servico_controller.router)
-api_router.include_router(funcionario_controller.router)
-api_router.include_router(ordem_servico_controller.router)
-api_router.include_router(item_servico_controller.router)
+api_router.include_router(cliente_router.router)
+api_router.include_router(funcionario_router.router)
+api_router.include_router(item_servico_router.router)
+api_router.include_router(ordem_servico_router.router)
+api_router.include_router(servico_router.router)
