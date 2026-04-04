4c8f33724d3b654e2def705415759f814bc12dae
1llucaz <lucasemanueloliveiraaraujo@gmail.com>
2026-04-04 13:02:35 -0300

    fix (cliente_repository): correção de consultas via ORM no repository e implementação atualizada do SQLAlchemy

--- app/modules/cliente/cliente_repository.py
+++ app/modules/cliente/cliente_repository.py
@@ -1,19 +1,20 @@
 from app.modules.cliente.cliente_model import Cliente
 from sqlalchemy.orm import Session
+from sqlalchemy import select, exists
+
 
 class ClienteRepository:
 
     def __init__ (self, db: Session):
         self.db = db
 
-    def salvar_cliente(self, cliente: Cliente) -> Cliente:
+    def registrar_cliente(self, cliente: Cliente) -> Cliente:
         self.db.add(cliente)
-        self.db.flush()
         return cliente
 
     def atualizar_cliente(self,
                             id: int,
-                            dados_novos: dict) -> type[Cliente] | None:
+                            dados_novos: dict) -> Cliente | None:
 
         if id is None or dados_novos is None:
             return None
@@ -27,83 +28,108 @@ class ClienteRepository:
             if hasattr(cliente_antigo, campo):
                 setattr(cliente_antigo, campo, valor)
 
-        self.db.flush()
         return cliente_antigo
 
+    def buscar_um     (self,
+                       nome: str | None = None,
+                       email: str | None = None,
+                       telefone: str | None = None,
+                       ativo: bool | None = None) -> Cliente | None:
 
-    def busca_dinamica (self,
-                         nome: str | None = None,
-                         email: str | None = None,
-                         telefone: str | None = None,
-                         ativo: bool | None = None) -> list [Cliente]:
+        condicionais = {campo: dado for campo, dado in locals().items() if dado is not None and campo != "self"}
 
-
-        query = self.db.query(Cliente)
-
-        condicionais : dict = {"nome": nome, "email": email, "telefone": telefone, "ativo": ativo}
+        consulta = select(Cliente)
 
         for campo, dado in condicionais.items():
 
             if dado is None:
-                continue
+                pass
 
-            if campo == "nome":
-                coluna = getattr(Cliente, campo)
-                query = query.filter(coluna.ilike(f"%{dado}%"))
+            elif campo == "nome":
+                consulta = consulta.where(Cliente.nome.ilike(f"%{dado}%"))
+
+            elif campo == "ativo":
+                consulta = consulta.where(Cliente.ativo.is_(dado))
 
             else:
-                query = query.filter_by(**{campo: dado})
+                coluna = getattr(Cliente, campo)
+                consulta = consulta.where(coluna == dado)
 
-        return query.all()
+        if not condicionais:
+            return None
 
-    def
+        return self.db.execute(consulta).scalar_one_or_none()
+
+    def buscar_varios  (self,
+                         nome: str | None = None,
+                         email: str | None = None,
+                         telefone: str | None = None,
+                         ativo: bool | None = None) -> list [Cliente] | None:
+
+        condicionais = {campo:dado for campo, dado in locals().items() if dado is not None and campo != "self"}
+
+        consulta = select(Cliente)
+
+        for campo, dado in condicionais.items():
+
+            if campo == "nome":
+                consulta = consulta.where(Cliente.nome.ilike(f"%{dado}%"))
+
+            elif campo == "ativo":
+                consulta = consulta.where(Cliente.ativo.is_(dado))
+
+            else:
+                coluna = getattr (Cliente, campo)
+                consulta = consulta.where(coluna == dado)
+
+        if not condicionais:
+            return None
+
+        return self.db.execute(consulta).scalars().all()
 
 
     '''
-    a busca dinâmica pode buscar um ou vários clientes e, devido a confiabilidade, a função desativar foi limitada
+    a buscar_varios pode buscar um ou vários clientes e, devido a confiabilidade, a função desativar foi limitada
     para efetuar isso apenas quando for retornado um único e inequívoco registro, evitando que mais de um cliente possa 
     ser desativado a cada requisição. Ass: Lucas
     '''
 
+    def buscar_todos (self) -> list[Cliente] | None:
+        return self.db.execute(select(Cliente)).scalars().all()
+
     def desativar_cliente(self,
-                            id: int | None = None,
-                            email: str | None = None,
-                            telefone: str | None = None) -> type[Cliente] | None:
+                          id: int | None = None,
+                          email: str | None = None,
+                          telefone: str | None = None) -> Cliente | None:
 
-        condicionais = {"id": id, "email": email, "telefone": telefone}
+        condicionais = {campo: dado for campo, dado in locals().items() if dado is not None and campo != "self"}
 
-        if email is None and telefone is None and id is None:
+        if not condicionais:
             return None
 
-        if id is not None:
+        if id:
             cliente = self.db.get(Cliente, id)
 
-        else:
+            if cliente is None:
+                return None
 
-            cliente = self.db.query(Cliente)
-
-            for campo, dado in condicionais.items():
-                if dado is None:
-                    continue
-
-                else:
-                    cliente = cliente.query(Cliente).filter_by(**{campo: dado})
-
-            cliente = cliente.first()
-
-        if cliente is None:
-            return None
-
-        if not cliente.ativo:
-            return Cliente
-
-        else:
             cliente.ativo = False
-            self.db.flush()
             return cliente
 
 
-    def buscar_por_id (self, id: int) -> type[Cliente] | None:
+        elif id is None:
+            cliente = self.buscar_um(email=email, telefone=telefone)
+
+            if cliente is None:
+                return None
+
+            cliente.ativo = False
+
+            return cliente
+
+        return None
+
+    def buscar_por_id (self, id: int) -> Cliente | None:
 
         if id is None:
             return None
@@ -118,7 +144,9 @@ class ClienteRepository:
 
 
     def exists_email (self, email:str | None = None) -> bool:
-        return self.db.query(Cliente).filter_by(email=email).exists().scalar()
+        consulta = select(exists().where(Cliente.email == email))
+        return self.db.execute(consulta).scalar()
 
     def exists_telefone(self, telefone: str | None = None) -> bool:
-        return self.db.query(Cliente).filter_by(telefone=telefone).exists().scalar()
+        consulta = select(exists().where(Cliente.telefone == telefone))
+        return self.db.execute(consulta).scalar()
