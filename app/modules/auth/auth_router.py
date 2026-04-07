ee3c4f48b8a9a85318564452e7bfe3154a85415f
1llucaz <lucasemanueloliveiraaraujo@gmail.com>
2026-04-07 17:40:29 -0300

    refactor

--- app/modules/auth/auth_router.py
+++ app/modules/auth/auth_router.py
@@ -11,8 +11,8 @@ router = APIRouter(tags=["Autenticação"])
 @router.post("/login",
              status_code=status.HTTP_200_OK)
 def login(is_colaborador: bool,
-                  form: OAuth2PasswordRequestForm = Depends(),
-                  service: AuthService = Depends(get_auth_service)):
-    return service.login (is_colaborador = is_colaborador,
-                                 email=form.username,
-                                 senha=form.password)
+          form: OAuth2PasswordRequestForm = Depends(),
+          service: AuthService = Depends(get_auth_service)):
+    return service.login(is_colaborador=is_colaborador,
+                         email=form.username,
+                         senha=form.password)
--- main.py
+++ main.py
@@ -1,9 +1,5 @@
 from fastapi import FastAPI
 
-from app.core.dependencies import get_funcionario_service
-from app.core.security import generate_password_hash
-from app.modules.funcionario.funcionario_model import Funcionario
-from app.modules.funcionario.funcionario_repository import FuncionarioRepository
 from app.routers.api_router import api_router
 from fastapi.middleware.cors import CORSMiddleware
 
@@ -24,12 +20,3 @@ app.add_middleware(
 def root():
     return {"HI": "Hello World"}
 
-
-funcionario_master: Funcionario = Funcionario(nome="MasterClass",
-                                              email="master@gmail.com",
-                                              senha=generate_password_hash("123"),
-                                              is_admin=True, access_funcionario=True,
-                                              is_colaborador=True, access_servico=True,
-                                              access_cliente=True, access_item_servico=True,
-                                              access_ordem_servico=True, ativo=True,
-                                              cargo="MasterClass", )
