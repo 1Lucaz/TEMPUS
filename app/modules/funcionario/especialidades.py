1b20ffc0b3f87a33681e55fd72bae84c6b5b4599
1llucaz <lucasemanueloliveiraaraujo@gmail.com>
2026-04-11 00:43:04 -0300

    delete: retirada dos arquivos especialidades e funcionario_especialidade

--- app/modules/funcionario/especialidades.py
+++
@@ -1,8 +0,0 @@
-from sqlalchemy.orm import MappedAsDataclass, DeclarativeBase
-
-
-class Base (MappedAsDataclass, DeclarativeBase):
-    pass
-
-class Especialidade(Base):
-    __tablename__ = 'especialidade'
