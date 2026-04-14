e23834476ebcef9b5c28cc249b815fc996bd3ba7
1llucaz <lucasemanueloliveiraaraujo@gmail.com>
2026-04-14 11:31:22 -0300

    feat: banco torna-se assíncrono

--- app/core/database.py
+++ app/core/database.py
@@ -1,28 +1,34 @@
 import os
-
+import re
 from dotenv import load_dotenv
-
-from sqlalchemy import create_engine, inspect
-from sqlalchemy.orm import Session, sessionmaker
-
 from app.core.base import Base
+from sqlalchemy.ext.asyncio import AsyncSession, async_sessionmaker
+from sqlalchemy.ext.asyncio.engine import create_async_engine
 
-#dados do db encontram-se arquivo properties.py
 load_dotenv()
-engine = create_engine (os.getenv('DATABASE_URL'), pool_pre_ping=True, pool_recycle=3600)
-SessionLocal = sessionmaker (autocommit=False, autoflush=False, bind=engine)
+engine = create_async_engine(re.sub
+                             (r'^postgresql:',
+                              'postgresql+psycopg:',
+                              os.getenv('DATABASE_URL')),
+                             echo=True)
 
-'''def init_db():
-    Base.metadata.drop_all(engine)
-    Base.metadata.create_all(bind=engine)
-    print("BANCO DE DADOS CRIADO")'''
+
+async def get_session() -> AsyncSession | None:
+    async with AsyncSession(engine, expire_on_commit=False) as session:
+        yield session
+
+
+AsyncLocalSession = async_sessionmaker(bind=get_session(),
+                                       expire_on_commit=False,
+                                       autoflush=True)
+
 
 class Database:
-    def __enter__ (self):
-        self.session = SessionLocal()
+    def __enter__(self):
+        self.session = AsyncLocalSession()
         return self.session
 
-    def __exit__ (self, exc_type, exc_value, exc_traceback):
+    def __exit__(self, exc_type, exc_value, exc_traceback):
 
         try:
             if exc_type:
@@ -41,6 +47,6 @@ class Database:
             self.session.close()
 
 
-async def get_db ():
+async def get_db():
     with Database() as db:
         yield db
