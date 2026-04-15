8cc7b0ad0c93e8d2cfb9a1e568131d4581613bf6
Geronimo da Silva Nasc. <122554776+Geronimo1217@users.noreply.github.com>
2026-04-14 23:09:26 -0300

    Update categoria_schema.py

--- app/modules/categoria_schema.py
+++ app/modules/categoria_schema.py
@@ -1,16 +1,28 @@
 from pydantic import BaseModel
 
-class CategoriaServicoResponse(BaseModel):
+class CategoriaResponse(BaseModel):
     id: int
     descricao: str
     ativo: bool
+    created_at: datetime
+    updated_at: datetime | None
+    
     model_config = {"from_attributes": True}
 
 
-class CategoriaServicoCreate(BaseModel):
+class CategoriaCreate(BaseModel):
     id: int
-    descricao: str
-    ativo: bool = True
+    descricao: str  | None = None
+    ativo: bool = True 
+
+    model_config = {"from_attributes": True}
+    
+    
+   
+
+class CategoriaUpdate(BaseModel):
+    descricao: str | None = None
+    ativo: bool | None = None
 
     model_config = {"from_attributes": True}
     
