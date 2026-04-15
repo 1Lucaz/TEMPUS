c87de6794289a944da6c17d0dcf9d9694c5d3b54
Geronimo da Silva Nasc. <122554776+Geronimo1217@users.noreply.github.com>
2026-04-14 23:10:50 -0300

    Update categoria_model.py

--- app/modules/categoria_model.py
+++ app/modules/categoria_model.py
@@ -1,6 +1,6 @@
 
 from datetime import datetime
-from sqlalchemy import String, Boolean, Integer, func
+from sqlalchemy import String,BIGINT, Boolean, Integer, func
 from sqlalchemy.orm import Mapped, mapped_column
 
 from app.core.base import Base
